4e8ed51b7fbedd65173abd8296ea6abff282b2d4
Omotayo <kehindeomotayo11@gmail.com>
2023-09-22 12:24:43 +0100

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    // Fetch questions when the application loads
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((data) => setQuestions(data))
+      .catch((error) => console.error("Error fetching questions:", error));
+  }, []);
+
+  const handleDeleteQuestion = (id) => {
+    // Remove the deleted question from state
+    setQuestions((prevQuestions) =>
+      prevQuestions.filter((question) => question.id !== id)
+    );
+  };
+
+  const handleUpdateQuestion = (id, newCorrectIndex) => {
+    // Update the correctIndex for the question in state
+    setQuestions((prevQuestions) =>
+      prevQuestions.map((question) =>
+        question.id === id
+          ? { ...question, correctIndex: newCorrectIndex }
+          : question
+      )
+    );
+  };
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? (
+        <QuestionForm />
+      ) : (
+        <QuestionList
+          questions={questions}
+          onDelete={handleDeleteQuestion}
+          onUpdate={handleUpdateQuestion}
+        />
+      )}
     </main>
   );
 }
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onUpdate }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,13 +9,46 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  const [selectedCorrectIndex, setSelectedCorrectIndex] =
+    useState(correctIndex);
+
+  const handleCorrectIndexChange = (event) => {
+    const newCorrectIndex = parseInt(event.target.value);
+    setSelectedCorrectIndex(newCorrectIndex);
+
+    // Send PATCH request to update the correct answer on the server
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        correctIndex: newCorrectIndex,
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          // If the update request is successful, trigger onUpdate to update the question in the state
+          onUpdate(id, newCorrectIndex);
+        } else {
+          console.error("Error updating question");
+        }
+      })
+      .catch((error) => console.error("Error updating question:", error));
+  };
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select
+          value={selectedCorrectIndex}
+          onChange={handleCorrectIndexChange}
+        >
+          {options}
+        </select>
       </label>
       <button>Delete Question</button>
     </li>
